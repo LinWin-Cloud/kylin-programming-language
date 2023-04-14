ff67e52b85e5cefcc4b10468f6821afeace3218e
LinWin-Cloud <2382546858@qq.com>
2023-04-14 21:34:55 +0800

    Kylin runtime

---
+++ Kylin/src/main/java/com/kylin/Main.java
@@ -0,0 +1,48 @@
+package com.kylin;
+
+import com.kylin.Runtime.MainRuntime;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class Main {
+    public static File resource;
+    public static ArrayList<String> code = new ArrayList<>();
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("kylin [resource file] ");
+            System.exit(1);
+        }
+        resource = new File(args[0]);
+        if (!resource.exists()) {
+            System.out.println("[ERROR] Cannot find target file: "+resource.getAbsolutePath()+".");
+            System.exit(1);
+        }
+        if (resource.exists() && resource.isDirectory()) {
+            System.out.println("[ERROR] Cannot start a directory.");
+            System.exit(1);
+        }
+        try {
+            FileReader fileReader = new FileReader(resource);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            while (true) {
+                String line = bufferedReader.readLine();
+                if (line == null) {
+                    break;
+                }
+                code.add(line);
+            }
+            MainRuntime mainRuntime = new MainRuntime();
+            mainRuntime.setCode(code);
+            mainRuntime.run();
+        }
+        catch (Exception exception) {
+            System.out.println("[ERROR] Read target file error.");
+            System.exit(1);
+        }
+    }
+}
--- Kylin/src/main/java/com/kylin/Runtime/MainRuntime.java
+++ Kylin/src/main/java/com/kylin/Runtime/MainRuntime.java
@@ -1,2 +1,15 @@
-package com.kylin.Runtime;public class MainRuntime {
+package com.kylin.Runtime;
+
+import java.util.ArrayList;
+
+public class MainRuntime {
+    private ArrayList<String> code = new ArrayList<>();
+
+    public void setCode(ArrayList<String> arrayList) {
+        this.code = arrayList;
+    }
+
+    public void run() {
+        
+    }
 }
